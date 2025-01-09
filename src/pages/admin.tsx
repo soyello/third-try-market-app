3b97ac3fc389c0fdabe4a38abee2a85c92cb4709
soyello <wodus9193@naver.com>
2025-01-09 23:02:07 +0900

    NextAuth 적용 및 schema 생성

---
+++ src/pages/admin.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AdminPage = () => {
+  return <div>나는 행복한 홍삼</div>;
+};
+
+export default AdminPage;
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,87 @@
+import { RowDataPacket } from 'mysql2';
+import { NextAuthOptions, User } from 'next-auth';
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import pool from '../../../lib/db';
+
+declare module 'next-auth' {
+  interface User {
+    id: string;
+  }
+  interface Session {
+    user: User;
+  }
+  interface JWT {
+    id: string;
+  }
+}
+
+interface UserRow extends RowDataPacket {
+  id: string;
+  name: string;
+  email: string;
+  hashedPassword?: string;
+}
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text', placeholder: 'ID를 입력하세요' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const hardcodedUser = {
+          id: '정재연 천재',
+          name: '부자된다 일확천금',
+          email: 'hello@love.com',
+          hashedPassword: '12345',
+        };
+        if (!credentials) return null;
+        if (credentials.email === hardcodedUser.email && credentials.password === hardcodedUser.hashedPassword) {
+          return hardcodedUser as User;
+        }
+
+        const [rows] = await pool.query<UserRow[]>(
+          'SELECT id, name, email, hashedPassword FROM users WHERE email = ?',
+          [credentials.email]
+        );
+
+        const user = rows[0];
+
+        if (user && user.hashedPassword) {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user = {
+          ...(session.user || {}),
+          id: token.id as string,
+        };
+      }
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
