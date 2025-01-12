43391e3827ae6e1f5b9510c10c6ebdec3dad5a3f
soyello <wodus9193@naver.com>
2025-01-12 22:28:00 +0900

    상품 상세 페이지 UI 생성 시작

---
+++ src/components/product/ProductHead.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductHead = () => {
+  return <div></div>;
+};
+
+export default ProductHead;
--- src/pages/products/[productId].tsx
+++ src/pages/products/[productId].tsx
@@ -1,19 +1,42 @@
+import Button from '@/components/Button';
+import ProductHead from '@/components/product/ProductHead';
+import ProductInfo from '@/components/product/ProductInfo';
 import { serializedProductWithUser } from '@/helper/serialization';
+import { Product } from '@/helper/type';
 import getProductById from '@/lib/getProductById';
+import { AdapterUser } from 'next-auth/adapters';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 
 interface Params {
   productId?: string;
 }
 
-const ProductPage = ({ product, creator }: { product: any; creator: any }) => {
+interface ProductclientProps {
+  product: Product | null;
+  creator: AdapterUser | null;
+}
+
+const ProductPage = ({ product, creator }: ProductclientProps) => {
+  const router = useRouter();
+  const KakaoMap = dynamic(() => import('../../components/KakaoMap'), { ssr: false });
   if (!product) {
     return <div>Product not found</div>;
   }
   return (
-    <div>
-      <h1>{product.title}</h1>
-      <p>{product.description}</p>
-      <p>Posted by: {creator || 'Guest'}</p>
+    <div className='max-w-screen-lg mx-auto'>
+      <div className='flex flex-col gap-6'>
+        <ProductHead />
+        <div className='grid grid-cols-1 mt-6 md:grid-cols-2 md:gap-10'>
+          <ProductInfo />
+          <div>
+            <KakaoMap detailPage latitude={product.latitude} longitude={product.longitude} />
+          </div>
+        </div>
+      </div>
+      <div className='mt-10'>
+        <Button label='이 유저와 채팅하기' onClick={() => router.push('/chat')} />
+      </div>
     </div>
   );
 };
@@ -23,18 +46,26 @@ export default ProductPage;
 export async function getServerSideProps(context: { params: Params; req: any; res: any }) {
   const { params, req, res } = context;
 
-  const productId = params.productId;
-  if (!productId) {
+  try {
+    const productId = params.productId;
+    if (!productId) {
+      return {
+        notFound: true,
+      };
+    }
+
+    const product = await getProductById({ productId });
+    const serializedProduct = serializedProductWithUser(product);
+    return {
+      props: {
+        product: serializedProduct,
+        creator: product?.user.name,
+      },
+    };
+  } catch (error) {
+    console.error('Error in getServerSideProps:', error);
     return {
       notFound: true,
     };
   }
-  const product = await getProductById({ productId });
-  const serializedProduct = serializedProductWithUser(product);
-  return {
-    props: {
-      product: serializedProduct,
-      creator: product!.user.name,
-    },
-  };
 }
